cf3416f60e410d48b392786a3a97373d085d3183
Ava7 <avalanche_1991@hotmail.com>
2015-02-05 20:37:00 +0200

    Nau4i.me custom videos migration script

--- php/resumable_upload.php
+++ php/resumable_upload.php
@@ -1,8 +1,8 @@
 <?php
-
+ini_set('max_execution_time', 86400);
 // Call set_include_path() as needed to point to your client library.
-require_once 'Google/Client.php';
-require_once 'Google/Service/YouTube.php';
+require_once '../../google-api-php-client/src/Google/Client.php';
+require_once '../../google-api-php-client/src/Google/Service/YouTube.php';
 session_start();
 
 /*
@@ -19,8 +19,7 @@ $client = new Google_Client();
 $client->setClientId($OAUTH2_CLIENT_ID);
 $client->setClientSecret($OAUTH2_CLIENT_SECRET);
 $client->setScopes('https://www.googleapis.com/auth/youtube');
-$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
-    FILTER_SANITIZE_URL);
+$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'], FILTER_SANITIZE_URL);
 $client->setRedirectUri($redirect);
 
 // Define an object that will be used to make all API requests.
@@ -42,77 +41,97 @@ if (isset($_SESSION['token'])) {
 
 // Check to ensure that the access token was successfully acquired.
 if ($client->getAccessToken()) {
-  try{
-    // REPLACE this value with the path to the file you are uploading.
-    $videoPath = "/path/to/file.mp4";
+  try {
 
-    // Create a snippet with title, description, tags and category ID
-    // Create an asset resource and set its snippet metadata and type.
-    // This example sets the video's title, description, keyword tags, and
-    // video category.
-    $snippet = new Google_Service_YouTube_VideoSnippet();
-    $snippet->setTitle("Test title");
-    $snippet->setDescription("Test description");
-    $snippet->setTags(array("tag1", "tag2"));
+    $dsn = 'mysql:dbname=nau4i;host=127.0.0.1';
+    $user = 'root';
+    $password = '';
 
-    // Numeric video category. See
-    // https://developers.google.com/youtube/v3/docs/videoCategories/list 
-    $snippet->setCategoryId("22");
-
-    // Set the video's status to "public". Valid statuses are "public",
-    // "private" and "unlisted".
-    $status = new Google_Service_YouTube_VideoStatus();
-    $status->privacyStatus = "public";
-
-    // Associate the snippet and status objects with a new video resource.
-    $video = new Google_Service_YouTube_Video();
-    $video->setSnippet($snippet);
-    $video->setStatus($status);
-
-    // Specify the size of each chunk of data, in bytes. Set a higher value for
-    // reliable connection as fewer chunks lead to faster uploads. Set a lower
-    // value for better recovery on less reliable connections.
-    $chunkSizeBytes = 1 * 1024 * 1024;
-
-    // Setting the defer flag to true tells the client to return a request which can be called
-    // with ->execute(); instead of making the API call immediately.
-    $client->setDefer(true);
-
-    // Create a request for the API's videos.insert method to create and upload the video.
-    $insertRequest = $youtube->videos->insert("status,snippet", $video);
-
-    // Create a MediaFileUpload object for resumable uploads.
-    $media = new Google_Http_MediaFileUpload(
-        $client,
-        $insertRequest,
-        'video/*',
-        null,
-        true,
-        $chunkSizeBytes
-    );
-    $media->setFileSize(filesize($videoPath));
-
-
-    // Read the media file and upload it chunk by chunk.
-    $status = false;
-    $handle = fopen($videoPath, "rb");
-    while (!$status && !feof($handle)) {
-      $chunk = fread($handle, $chunkSizeBytes);
-      $status = $media->nextChunk($chunk);
+    try {
+        $dbh = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES "UTF8"'));
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
     }
+    $query = $dbh->query("SELECT `videos`.`id`, `videos`.`name` AS `video`, `content`.`introtext`, `content`.`title`
+        FROM `videos`
+        LEFT JOIN `content` ON `videos`.`content_id` = `content`.`id`
+        LIMIT 0, 5");
+    // var_dump($dbh->errorInfo());
 
-    fclose($handle);
+    // print_r($query->fetchAll(PDO::FETCH_ASSOC));
+    foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $vid) {
+        $videoPath = preg_replace('~(http://nau4i.me)~', '..', $vid['video']);
+        $description = stripslashes(strip_tags(html_entity_decode($vid['introtext'])));
+        $description = preg_replace('~{flowplayer}.+{/flowplayer}~', '', $description);
 
-    // If you want to make other calls after the file upload, set setDefer back to false
-    $client->setDefer(false);
+        // $videoPath = "meow.mp4";
+
+        $snippet = new Google_Service_YouTube_VideoSnippet();
+        $snippet->setTitle($vid['title']);
+        $snippet->setDescription($description);
+        $snippet->setTags(array('php', 'mysql', 'web', 'Научи ме', 'nau4i.me'));
+
+        // Numeric video category. See
+        // https://developers.google.com/youtube/v3/docs/videoCategories/list
+        $snippet->setCategoryId("28");
+
+        // Set the video's status to "public". Valid statuses are "public",
+        // "private" and "unlisted".
+        $status = new Google_Service_YouTube_VideoStatus();
+        $status->privacyStatus = "unlisted";
+
+        // Associate the snippet and status objects with a new video resource.
+        $video = new Google_Service_YouTube_Video();
+        $video->setSnippet($snippet);
+        $video->setStatus($status);
+
+        // Specify the size of each chunk of data, in bytes. Set a higher value for
+        // reliable connection as fewer chunks lead to faster uploads. Set a lower
+        // value for better recovery on less reliable connections.
+        $chunkSizeBytes = 1 * 1024 * 1024;
+
+        // Setting the defer flag to true tells the client to return a request which can be called
+        // with ->execute(); instead of making the API call immediately.
+        $client->setDefer(true);
+
+        // Create a request for the API's videos.insert method to create and upload the video.
+        $insertRequest = $youtube->videos->insert("status,snippet", $video);
+
+        // Create a MediaFileUpload object for resumable uploads.
+        $media = new Google_Http_MediaFileUpload(
+            $client,
+            $insertRequest,
+            'video/*',
+            null,
+            true,
+            $chunkSizeBytes
+        );
+        $media->setFileSize(filesize($videoPath));
 
 
-    $htmlBody .= "<h3>Video Uploaded</h3><ul>";
-    $htmlBody .= sprintf('<li>%s (%s)</li>',
-        $status['snippet']['title'],
-        $status['id']);
+        // Read the media file and upload it chunk by chunk.
+        $status = false;
+        $handle = fopen($videoPath, "rb");
+        while (!$status && !feof($handle)) {
+          $chunk = fread($handle, $chunkSizeBytes);
+          $status = $media->nextChunk($chunk);
+        }
 
-    $htmlBody .= '</ul>';
+        fclose($handle);
+
+        $dbh->query("UPDATE `videos` SET `youtube` = '{$status['id']}' WHERE `id` = '{$vid['id']}'");
+
+        // If you want to make other calls after the file upload, set setDefer back to false
+        $client->setDefer(false);
+
+
+        $htmlBody = "<h3>Video Uploaded</h3><ul>";
+        $htmlBody .= sprintf('<li>%s (%s)</li>',
+            $status['snippet']['title'],
+            $status['id']);
+
+        $htmlBody .= '</ul>';
+    }
 
   } catch (Google_ServiceException $e) {
     $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
